cd2cad4d0286207324a0bdd6581e44b5a2f02c80
cmd69 <nasamigra@gmail.com>
2026-04-13 19:36:01 +0000

    [41]: TWA Fix

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -60,5 +60,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico)$).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|manifest\\.json|sw\\.js|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico)$).*)"],
 };
